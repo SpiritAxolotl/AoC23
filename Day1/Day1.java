08854ca5f3eae1eebdd1904b78dffad63c013d6e
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2023-12-01 14:19:16 -0700

    Update Day1.java again
    more minimizing lmao

--- Day1/Day1.java
+++ Day1/Day1.java
@@ -10,9 +10,8 @@ public class App {
             for (int j=0; j<numbers.length; j++) {
                 int n = numbers[j].length();
                 if (i>=length-n) continue;
-                if (in.substring(i,i+n).equals(numbers[j])) {
+                if (in.substring(i,i+n).equals(numbers[j]))
                     return numbers[j];
-                }
             }
         }
         return "";
@@ -23,17 +22,15 @@ public class App {
             for (int j=0; j<numbers.length; j++) {
                 int n = numbers[j].length();
                 if (i>=length-n) continue;
-                if (in.substring(length-(i+n),length-i).equals(numbers[j])) {
+                if (in.substring(length-(i+n),length-i).equals(numbers[j]))
                     return numbers[j];
-                }
             }
         }
         return "";
     }
     public static int indexOfArray(String match, String[] array) {
-        for (int i=0; i<array.length; i++) {
+        for (int i=0; i<array.length; i++)
             if (array[i].equals(match)) return i;
-        }
         return -1;
     }
     public static int[] numberParse(Scanner scan) {
@@ -47,9 +44,7 @@ public class App {
             for (int i=0; i<input.length(); i++) {
                 try {
                     ints1[0] = Integer.parseInt(input.substring(i,i+1));
-                } catch (NumberFormatException e) {
-                    continue;
-                }
+                } catch (NumberFormatException e) {continue;}
                 String lowestNum = lowestNum(input);
                 if (!lowestNum.isEmpty() && input.indexOf(lowestNum) < input.indexOf(ints1[0]+""))
                     ints2[0] = indexOfArray(lowestNum, numbers) + 1;
@@ -59,9 +54,7 @@ public class App {
             for (int i=input.length()-1; i>=0; i--) {
                 try {
                     ints1[1] = Integer.parseInt(input.substring(i,i+1));
-                } catch (NumberFormatException e) {
-                    continue;
-                }
+                } catch (NumberFormatException e) {continue;}
                 String highestNum = highestNum(input);
                 if (!highestNum.isEmpty() && input.lastIndexOf(highestNum) > input.lastIndexOf(ints1[1]+""))
                     ints2[1] = indexOfArray(highestNum, numbers) + 1;
@@ -75,9 +68,7 @@ public class App {
                     if (ints1[0] * ints1[1] == 0) System.out.print("ERROR ON ");
                     System.out.println("line " + c + ": " + ints2[0] + " " + ints2[1] + " becomes " + Integer.parseInt(ints2[0] + "" + ints2[1]));
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            } catch (Exception e) {e.printStackTrace();}
         }
         scan.close();
         return new int[] {total1, total2};
