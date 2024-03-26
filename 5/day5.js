57cf22300561f00c4511b1c578bf06cfa250bd38
Spax <gamecrusher218@gmail.com>
2024-03-26 11:45:44 -0600

    day 5 setup and template

---
+++ 5/day5.js
@@ -0,0 +1,12 @@
+const t = require("../global/test.js");
+
+function day5(str) {
+    const answer = [0, 0];
+    const lines = str.split("\n");
+    for (const line of lines) {
+        
+    }
+    return answer;
+}
+
+t.test(5, day5, [35, 35]);
---
+++ global/template.js
@@ -0,0 +1,12 @@
+const t = require("../global/test.js");
+
+function dayX(str) {
+    const answer = [0, 0];
+    const lines = str.split("\n");
+    for (const line of lines) {
+        
+    }
+    return answer;
+}
+
+t.test(x, dayX, [10, 10]);
