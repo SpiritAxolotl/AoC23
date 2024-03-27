c52cbd388c1a73c0ae3408e5ce52ad51514780bf
Spax <gamecrusher218@gmail.com>
2024-03-27 13:58:32 -0600

    this is super inefficient I need to find a better way to do this

--- 5/day5.js
+++ 5/day5.js
@@ -4,17 +4,35 @@ require("../global/day.js");
 class Day5 extends Day {
     constructor(str) {
         super(str);
-        this.seeds = this.lines[0].substring(this.lines[0].indexOf(":")+2).split(/\s+/g).map(i=>+i);
+        this.seedsToLocation = [];
+        this.seedsToLocation.push(this.lines[0].substring(this.lines[0].indexOf(":")+2).split(/\s+/g).map(i=>+i));
+        //this.seedsToLocation[0].sort((a,b)=>a-b);
         const lines = this.lines.slice(2);
-        this.map = [];
+        this.almanac = [];
         {
             let index = -1;
             for (const line of lines)
                 if (line.includes("map:"))
-                    this.map[++index] ??= [];
+                    this.almanac[++index] ??= [];
                 else if (line !== "")
-                    this.map[index].push(line.split(/\s+/g));
+                    this.almanac[index].push(line.split(/\s+/g).map(i=>+i));
         }
+        {
+            let index = 0;
+            for (const row of this.almanac) {
+                const order = [...this.seedsToLocation[index++]];
+                const neworder = [...order];
+                for (const [destStart, sourceStart, length] of row) {
+                    for (let i=0; i<length; i++) {
+                        const d = order.indexOf(sourceStart+i);
+                        if (d !== -1)
+                            neworder[d] = destStart+i;
+                    }
+                }
+                this.seedsToLocation[index] = neworder;
+            }
+        }
+        this.answer[0] = Math.min(...this.seedsToLocation[this.seedsToLocation.length-1]);
     }
 }
 
