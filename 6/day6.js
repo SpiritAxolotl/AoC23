8ad94c6780e1c5f3e13ab9ba5ab54073a66556e3
Spax <gamecrusher218@gmail.com>
2024-03-28 11:29:37 -0600

    day 6 start

---
+++ 6/day6.js
@@ -0,0 +1,20 @@
+require("../global/test.js");
+require("../global/day.js");
+
+class Day6 extends Day {
+    constructor(str) {
+        super(str);
+        this.races = [];
+        for (const line of this.lines) {
+            const split = line.split(/\s+/g);
+            const measure = split[0].replaceAll(":","").toLowerCase();
+            for (let i=1; i<split.length; i++) {
+                this.races[i-1] ??= {};
+                this.races[i-1][measure] = +split[i];
+            }
+        }
+        //for (let held=0; held<this.races[0].time; held++) {}
+    }
+}
+
+test(6, Day6, [288, 0], false);
