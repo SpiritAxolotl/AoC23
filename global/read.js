6e4bc1f5610ebb6811968ab39837159a4142bbc8
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2024-08-30 22:54:37 -0600

    fix read.js

--- global/read.js
+++ global/read.js
@@ -1,7 +1,7 @@
 const read = (day) => {
   const fs = require('fs');
-  const testInput = fs.readFileSync(`${process.env.HOME}/Repositories/AoC23/${day}/testinput.txt`, 'utf8');
-  const input = fs.readFileSync(`${process.env.HOME}/Repositories/AoC23/${day}/input.txt`, 'utf8');
+  const testInput = fs.readFileSync(`./${day}/testinput.txt`, 'utf8');
+  const input = fs.readFileSync(`./${day}/input.txt`, 'utf8');
   return [testInput, input];
 };
 
